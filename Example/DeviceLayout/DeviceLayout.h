44ecd115fbfc5c83ed134bd846311ef05a9977b2
cruz <cruzdiary@gmail.com>
2017-05-12 01:32:09 +0900

    Support carthage

---
+++ Example/DeviceLayout/DeviceLayout.h
@@ -0,0 +1,19 @@
+//
+//  DeviceLayout.h
+//  DeviceLayout
+//
+//  Created by CruzDiary on 12/05/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceLayout.
+FOUNDATION_EXPORT double DeviceLayoutVersionNumber;
+
+//! Project version string for DeviceLayout.
+FOUNDATION_EXPORT const unsigned char DeviceLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceLayout/PublicHeader.h>
+
+
